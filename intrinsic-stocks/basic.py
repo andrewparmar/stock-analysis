36879cc48e7de146cef2b6e5bbecda77ee11c464
Andrew Parmar <andrew.parmar@gmail.com>
2017-07-24 19:59:39 -0700

    Adding intrinsic value calclator

---
+++ intrinsic-stocks/basic.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# import seaborn as sns
+# sns.set_style('whitegrid')
+
+import pandas_datareader.data as web
+import datetime
+
+start_date = datetime.datetime(2010, 1, 1)
+end_date = datetime.datetime(2017, 6, 30)
+
+APPL = pd.DataFrame(web.DataReader('AAPL', 'google', start_date, end_date))
+# print APPL
+# APPL = web.DataReader('AAPL')
+# APPL.sort_values()
+# APPL.loc[2] = [1,1,1,1,np.NaN]
+# s = APPL.iloc[3]
+# # print s
+# APPL.append(s, ignore_index=True)
+# print APPL
+
+plt.plot(APPL['Close'])
+
+plt.title('Apple Inc Ticker Price')
+plt.show()
